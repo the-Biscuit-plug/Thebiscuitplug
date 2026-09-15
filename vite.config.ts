74b710b489bc0174f340e7b40daf4907d2fd3122
rizaanbrown245-byte <rizaanbrown245@gmail.com>
2026-09-15 16:20:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base:'/thebiscuitplug/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
